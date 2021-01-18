196b143c3ed9e978d2d05cf31308ca2d1f4f6308
omri wallach <omriwallach1992@gmail.com>
2021-01-18 14:05:13 +0100

    implement ucb algorithm

--- UCB/main.py
+++ UCB/main.py
@@ -4,38 +4,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv',header=None)
-transactions = []
 
-for i in range(0, len(dataset)):
-    transactions.append([])
-    for j in range(0, len(dataset.iloc[i])):
-        if(pd.notna(dataset.iloc[i][j])):
-            transactions[i].append(dataset.iloc[i][j])
+# Implement model
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+total_rewards = 0
 
-# Training the model
-from apyori import UCB
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
 
-rules = UCB(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
-results = list(rules)
-
-
-## Putting the results well organised into a Pandas DataFrame
-def inspect(results):
-    lhs         = [tuple(result[2][0][0])[0] for result in results]
-    rhs         = [tuple(result[2][0][1])[0] for result in results]
-    supports    = [result[1] for result in results]
-    confidences = [result[2][0][2] for result in results]
-    lifts       = [result[2][0][3] for result in results]
-    return list(zip(lhs, rhs, supports, confidences, lifts))
-resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
-
-## Displaying the results non sorted
-resultsinDataFrame
-
-## Displaying the results sorted by descending lifts
-resultsinDataFrame.nlargest(n = 10, columns = 'Lift')
-
-print(resultsinDataFrame)
+    for i in range(0, d):
+        if (numbers_of_selections[i] > 0):
+            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+            delta_i = math.sqrt(3*math.log(n+1)/2*numbers_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if (upper_bound > max_upper_bound):
+            max_upper_bound = upper_bound
+            ad = i
+    
+    ads_selected.append(ad)
+    numbers_of_selections[ad] += 1
+    sums_of_rewards[ad] += dataset.values[n, ad]
+    total_rewards += dataset.values[n, ad]
