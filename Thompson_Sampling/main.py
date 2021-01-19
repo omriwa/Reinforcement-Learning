373ef3309db683386af155e107be0858cc87a5a9
omri wallach <omriwallach1992@gmail.com>
2021-01-19 12:44:22 +0100

    thomponson sampling implementation

--- Thompson_Sampling/main.py
+++ Thompson_Sampling/main.py
@@ -9,31 +9,31 @@ import pandas as pd
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Implementing Thompson Sampeling
-import math
+import random
 N = 10000
 d = 10
 ads_selected = []
-numbers_of_selections = [0] * d
-sums_of_rewards = [0] * d
+number_of_rewards_0 = [0] * d
+number_of_rewards_1 = [0] * d
 total_reward = 0
+
 for n in range(0, N):
     ad = 0
-    max_upper_bound = 0
+    max_random = 0
+
     for i in range(0, d):
-        if (numbers_of_selections[i] > 0):
-            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
-            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
-            upper_bound = average_reward + delta_i
-        else:
-            upper_bound = 1e400
-        if upper_bound > max_upper_bound:
-            max_upper_bound = upper_bound
+        random_beta = random.betavariate(number_of_rewards_1[i] + 1, number_of_rewards_0[i] + 1)
+
+        if (max_random < random_beta):
+            max_random = random_beta
             ad = i
+       
     ads_selected.append(ad)
-    numbers_of_selections[ad] = numbers_of_selections[ad] + 1
     reward = dataset.values[n, ad]
-    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
-    total_reward = total_reward + reward
+    if (reward == 0):
+        number_of_rewards_0[ad] += 1
+    else:
+        number_of_rewards_1[ad] += 1
 
 # Visualising the results
 plt.hist(ads_selected)
